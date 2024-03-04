8b33d4eda13b8c74d9d3f771a0e8e74dcf73a245
ricardo fuentes <richard45oen@gmail.com>
2024-03-04 13:52:51 -0600

    mycode

--- index.js
+++ index.js
@@ -18,8 +18,63 @@ app.get('/api', (req, res) => {
     res.send('Hello World!');
 });
 
-// get all pets from the database
-app.get('/api/v1/pets', (req, res) => {
+// get all pets from the database 
+
+app.get('/api/v1/pets', (req, res) => { // i copy and pasted this.
+    res.send({pets: [
+        {
+            id: 1,
+            name: 'Fido',
+            breed: 'Labrador',
+            age: 3,
+            owner: 'John',
+            telephone: '555-555-5555',
+            appointments: [
+    
+                {
+                    date: '01/01/2020',
+                    time: '10:00',
+                    reason: 'checkup'
+                },
+            ]
+        },
+        {
+            id: 2,
+            name: 'Spot',
+            breed: 'Poodle',
+            age: 2,
+            owner: 'Jane',
+            telephone: '555-555-5555',
+            appointments: [
+                {
+                    date: '01/01/2020',
+                    time: '10:00',
+                    reason: 'checkup'
+                },
+    
+            ]
+        },
+        {
+            id: 3,
+            name: 'Rover',
+            breed: 'Pitbull',
+            age: 1,
+            owner: 'John',
+            telephone: '555-555-5555',
+            appointments: [
+                {
+                    date: '01/01/2020',
+                    time: '10:00',
+                    reason: 'checkup'
+                },
+                {
+                    date: '01/01/2020',
+                    time: '10:00',
+                    reason: 'checkup'
+                }
+            ]
+        }
+    ]})
     // send the pets array as a response
 
 });
